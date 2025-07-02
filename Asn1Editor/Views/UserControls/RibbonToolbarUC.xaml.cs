3038cde527cd75b20596036781ee8c38f6c9b38e
Vadims Podāns <vpodans@sysadmins.lv>
2025-07-02 22:23:27 +0300

    Drafted ribbon toolbar

---
+++ Asn1Editor/Views/UserControls/RibbonToolbarUC.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SysadminsLV.Asn1Editor.Views.UserControls;
+/// <summary>
+/// Interaction logic for RibbonToolbarUC.xaml
+/// </summary>
+public partial class RibbonToolbarUC {
+    public RibbonToolbarUC() {
+        InitializeComponent();
+    }
+}
