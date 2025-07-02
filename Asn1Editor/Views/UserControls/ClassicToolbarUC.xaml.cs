0a72bba35e5cfeffbb11d5b8fa0af8bbdaf5804c
Vadims Podāns <vpodans@sysadmins.lv>
2025-07-02 22:39:21 +0300

    Extracted classic menu bar and toolbar into user control, so we can easily switch between classic and ribbon toolbars

---
+++ Asn1Editor/Views/UserControls/ClassicToolbarUC.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+
+namespace SysadminsLV.Asn1Editor.Views.UserControls;
+/// <summary>
+/// Interaction logic for ClassicToolbarUC.xaml
+/// </summary>
+public partial class ClassicToolbarUC {
+    public ClassicToolbarUC() {
+        InitializeComponent();
+    }
+    void onCloseClick(Object sender, RoutedEventArgs args) {
+        Window? myWindow = Window.GetWindow(this);
+        myWindow?.Close();
+    }
+}
