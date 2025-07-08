3a99165ae9c2b5b5896b3fea6b3a0d0dda9dc1fd
Vadims Podāns <vpodans@sysadmins.lv>
2025-07-08 11:05:30 +0300

    Adjusted classic and ribbon toolbars

--- Asn1Editor/Views/UserControls/RibbonToolbarUC.xaml.cs
+++ Asn1Editor/Views/UserControls/RibbonToolbarUC.xaml.cs
@@ -1,9 +1,19 @@
-﻿namespace SysadminsLV.Asn1Editor.Views.UserControls;
+﻿using System;
+using System.Windows;
+
+namespace SysadminsLV.Asn1Editor.Views.UserControls;
 /// <summary>
 /// Interaction logic for RibbonToolbarUC.xaml
 /// </summary>
 public partial class RibbonToolbarUC {
     public RibbonToolbarUC() {
         InitializeComponent();
+    }
+    void onCloseClick(Object sender, RoutedEventArgs args) {
+        Window? myWindow = Window.GetWindow(this);
+        myWindow?.Close();
+    }
+    void onRibbonExpandCollapseClick(Object sender, RoutedEventArgs args) {
+        Ribbon.IsMinimized = !Ribbon.IsMinimized;
     }
 }
