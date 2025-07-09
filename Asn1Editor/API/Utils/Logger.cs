5d7529339571b4d67f8f665bf5cdb126bf954e55
Vadims Podāns <vpodans@sysadmins.lv>
2025-07-09 21:33:28 +0300

    implemented old log flush on startup

--- Asn1Editor/API/Utils/Logger.cs
+++ Asn1Editor/API/Utils/Logger.cs
@@ -1,31 +1,42 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SysadminsLV.Asn1Editor.API.Utils; 
 
 class Logger : IDisposable {
-    readonly String       basePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"Sysadmins LV\Asn1Editor");
-    readonly StreamWriter sesstionStream;
+    readonly String _logDirectory;
+    readonly StreamWriter sessionStream;
 
-    public Logger() {
-        Directory.CreateDirectory(Path.Combine(basePath, "Logs"));
+    public Logger(String appDataDirectory) {
+        _logDirectory = Directory.CreateDirectory(Path.Combine(appDataDirectory, "Logs")).FullName;
         String dt = "Log-" + DateTime.Now.ToString("ddMMyyyyHHmmss");
-        sesstionStream = new StreamWriter(Path.Combine(basePath, "Logs", $"{dt}-{Process.GetCurrentProcess().Id}.log")) { AutoFlush = true };
+        sessionStream = new StreamWriter(Path.Combine(_logDirectory, $"{dt}-{Process.GetCurrentProcess().Id}.log")) { AutoFlush = true };
+        Task.Run(flushOldLogs);
     }
+    
+    void flushOldLogs() {
+        var directory = new DirectoryInfo(_logDirectory);
+        foreach (FileInfo? fileInfo in directory.EnumerateFiles().OrderByDescending(x => x.LastWriteTime).Skip(10)) {
+            fileInfo.Delete();
+        }
+    }
+    
     public void Write(String s) {
-        sesstionStream.WriteLine(s);
+        sessionStream.WriteLine(s);
     }
     public void Write(Exception e) {
         String dt = DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss");
-        sesstionStream.WriteLine($"[{dt}]" + " An exception has been thrown:");
-        Exception ex = e;
+        sessionStream.WriteLine($"[{dt}]" + " An exception has been thrown:");
+        Exception? ex = e;
         do {
-            sesstionStream.WriteLine($"\tError message: {ex.Message}\r\n\tStack trace:\r\n{ex.StackTrace.Replace("   ", "\t\t")}");
+            sessionStream.WriteLine($"\tError message: {ex.Message}\r\n\tStack trace:\r\n{ex.StackTrace.Replace("   ", "\t\t")}");
             ex = ex.InnerException;
         } while (ex != null);
     }
     public void Dispose() {
-        sesstionStream?.Dispose();
+        sessionStream?.Dispose();
     }
 }
