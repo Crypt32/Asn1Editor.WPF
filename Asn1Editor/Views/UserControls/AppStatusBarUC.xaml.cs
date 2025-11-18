cd7e98140f4d570ba0d1873e24770fd0825510e1
Vadims Podāns <vpodans@sysadmins.lv>
2025-11-18 19:08:43 +0200

    refactored MainWindow by organizing resources into user controls/dictionaries

---
+++ Asn1Editor/Views/UserControls/AppStatusBarUC.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SysadminsLV.Asn1Editor.Views.UserControls;
+/// <summary>
+/// Interaction logic for AppStatusBarUC.xaml
+/// </summary>
+public partial class AppStatusBarUC {
+    public AppStatusBarUC() {
+        InitializeComponent();
+    }
+}
---
+++ Asn1Editor/Views/UserControls/DocumentTabsUC.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using SysadminsLV.Asn1Editor.API.Interfaces;
+using SysadminsLV.Asn1Editor.API.ViewModel;
+
+namespace SysadminsLV.Asn1Editor.Views.UserControls;
+/// <summary>
+/// Interaction logic for DocumentTabsView.xaml
+/// </summary>
+public partial class DocumentTabsUC {
+    public DocumentTabsUC() {
+        InitializeComponent();
+    }
+
+    void onTabHeaderContextMenuOpening(Object sender, ContextMenuEventArgs e) {
+        if (DataContext is not IMainWindowVM mwvm) {
+            e.Handled = true;
+            
+            return;
+        }
+
+        var vm = (AsnDocumentHostVM)((FrameworkElement)sender).DataContext;
+        e.Handled = mwvm.SelectedTab != vm;
+    }
+}
