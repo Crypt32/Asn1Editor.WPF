c51ad107db0e56fb99e2034e0b340cf4ecdd465d
Vadims Podāns <vpodans@sysadmins.lv>
2025-11-22 12:49:26 +0200

    Added a notification item when document is opened on side-by-side mode elsewhere. Live updates are not yet supported.

---
+++ Asn1Editor.Wpf.Controls/Converters/BooleanToVisibilityReversedConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SysadminsLV.Asn1Editor.Controls.Converters;
+
+/// <summary>
+/// Provides a value converter that reverses the standard Boolean-to-Visibility conversion logic.
+/// </summary>
+/// <remarks>
+/// This converter returns <see cref="Visibility.Collapsed"/> when the input value is <c>false</c>,
+/// and <see cref="Visibility.Visible"/> when the input value is <c>true</c>.
+/// </remarks>
+public class BooleanToVisibilityReversedConverter : IValueConverter {
+    public Object? Convert(Object? value, Type targetType, Object? parameter, CultureInfo culture) {
+        return value is false
+            ? Visibility.Visible
+            : Visibility.Collapsed;
+    }
+    public Object? ConvertBack(Object? value, Type targetType, Object? parameter, CultureInfo culture) {
+        throw new NotImplementedException();
+    }
+}
