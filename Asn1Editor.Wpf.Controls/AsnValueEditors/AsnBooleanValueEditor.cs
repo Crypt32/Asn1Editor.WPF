a53e8895768be69e2516415654130383b34e4030
Vadims Podāns <vpodans@sysadmins.lv>
2025-11-23 14:50:33 +0200

    drafted some new WPF controls for ASN Tag Editor. Holding here until dependencies are refactored.

---
+++ Asn1Editor.Wpf.Controls/AsnValueEditors/AsnBooleanValueEditor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+
+namespace SysadminsLV.Asn1Editor.Controls;
+
+public class AsnBooleanValueEditor : AsnValueEditor {
+    public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+        nameof(Value),
+        typeof(Boolean?),
+        typeof(AsnBooleanValueEditor),
+        new FrameworkPropertyMetadata(null,
+            FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+            onValueChanged));
+
+    static void onValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+        var editor = (AsnBooleanValueEditor)d;
+        editor.OnValueChanged((Boolean?)e.NewValue);
+    }
+
+    public Boolean? Value {
+        get => (Boolean?)GetValue(ValueProperty);
+        set => SetValue(ValueProperty, value);
+    }
+
+    protected override void OnBinaryValueChanged(Byte[]? oldValue, Byte[]? newValue) {
+        if (newValue is not { Length: 1 }) {
+            SetValidationState(false, "BOOLEAN must be a single octet.");
+            Value = null;
+            
+            return;
+        }
+
+        Value = newValue[0] != 0;
+        SetValidationState(true);
+    }
+
+    void OnValueChanged(Boolean? newValue) {
+        if (newValue == null) {
+            BinaryValue = [0x00];
+            SetValidationState(true);
+            return;
+        }
+
+        BinaryValue = [newValue.Value ? (Byte)0xFF : (Byte)0x00];
+        SetValidationState(true);
+    }
+}
---
+++ Asn1Editor.Wpf.Controls/AsnValueEditors/AsnHexValueEditor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows;
+using SysadminsLV.Asn1Parser;
+
+namespace SysadminsLV.Asn1Editor.Controls;
+
+public class AsnHexValueEditor :AsnValueEditor {
+
+    public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+        nameof(Value),
+        typeof(String),
+        typeof(AsnHexValueEditor),
+        new FrameworkPropertyMetadata(String.Empty,
+            FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+            onValueChanged));
+    static void onValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+        var editor = (AsnHexValueEditor)d;
+        editor.OnValueChanged((String?)e.NewValue);
+    }
+
+    public String Value
+    {
+        get => (String)GetValue(ValueProperty);
+        set => SetValue(ValueProperty, value);
+    }
+
+    protected override void OnBinaryValueChanged(Byte[]? oldValue, Byte[]? newValue) {
+        Value = newValue is null
+            ? String.Empty
+            : AsnFormatter.BinaryToString(newValue, EncodingType.Hex);
+
+        SetValidationState(true);
+    }
+    void OnValueChanged(String? newValue) {
+        if (String.IsNullOrWhiteSpace(newValue)) {
+            BinaryValue = [];
+            SetValidationState(true);
+            
+            return;
+        }
+
+        Byte[] binary = null;
+
+        try {
+            binary = AsnFormatter.StringToBinary(newValue, EncodingType.Hex);
+        } catch (Exception ex) {
+            SetValidationState(false, ex.Message);
+        }
+
+        // if we reached this far, binary is not null
+        BinaryValue = binary!;
+        SetValidationState(true);
+    }
+}
