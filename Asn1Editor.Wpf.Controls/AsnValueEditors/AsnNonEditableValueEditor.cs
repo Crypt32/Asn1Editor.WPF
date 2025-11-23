bfd618039def893908d0c7d35ae4e4079a61d9e7
Vadims Podāns <vpodans@sysadmins.lv>
2025-11-23 14:50:33 +0200

    drafted some new WPF controls for ASN Tag Editor. Holding here until dependencies are refactored.

---
+++ Asn1Editor.Wpf.Controls/AsnValueEditors/AsnNonEditableValueEditor.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SysadminsLV.Asn1Editor.Controls;
+
+public class AsnNonEditableValueEditor : AsnValueEditor {
+    protected override void OnBinaryValueChanged(Byte[] oldValue, Byte[] newValue) {
+        // No editing allowed, do nothing
+    }
+}
