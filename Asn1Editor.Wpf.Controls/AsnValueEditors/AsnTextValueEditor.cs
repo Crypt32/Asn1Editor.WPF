fd12b09b2226c4f31d343836a9ecb5d3e3f3e761
Vadims Podāns <vpodans@sysadmins.lv>
2025-11-23 14:50:33 +0200

    drafted some new WPF controls for ASN Tag Editor. Holding here until dependencies are refactored.

---
+++ Asn1Editor.Wpf.Controls/AsnValueEditors/AsnTextValueEditor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+
+namespace SysadminsLV.Asn1Editor.Controls;
+
+public class AsnTextValueEditor : AsnValueEditor {
+    public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+        nameof(Value), typeof(String), typeof(AsnTextValueEditor), new PropertyMetadata(default(String)));
+
+    public String Value
+    {
+        get => (String)GetValue(ValueProperty);
+        set => SetValue(ValueProperty, value);
+    }
+    
+    protected override void OnBinaryValueChanged(Byte[] oldValue, Byte[] newValue) {
+        
+    }
+}
---
+++ Asn1Editor.Wpf.Controls/AsnValueEditors/AsnValueEditor.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SysadminsLV.Asn1Editor.Controls;
+
+public abstract class AsnValueEditor : Control {
+    #region BinaryValue
+
+    public static readonly DependencyProperty BinaryValueProperty = DependencyProperty.Register(
+        nameof(BinaryValue),
+        typeof(Byte[]),
+        typeof(AsnValueEditor),
+        new FrameworkPropertyMetadata(null,
+            FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+            onBinaryValueChanged));
+
+    static void onBinaryValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+        if (d is AsnValueEditor editor) {
+            editor.OnBinaryValueChanged((Byte[])e.OldValue, (Byte[])e.NewValue);
+        }
+    }
+
+    public Byte[] BinaryValue {
+        get => (Byte[])GetValue(BinaryValueProperty);
+        set => SetValue(BinaryValueProperty, value);
+    }
+
+    #endregion
+
+    #region IsValid
+
+    static readonly DependencyPropertyKey IsValidPropertyKey =
+        DependencyProperty.RegisterReadOnly(nameof(IsValid),
+            typeof(Boolean),
+            typeof(AsnValueEditor),
+            new PropertyMetadata(true));
+
+
+    public static readonly DependencyProperty IsValidProperty = IsValidPropertyKey.DependencyProperty;
+
+    public Boolean IsValid {
+        get => (Boolean)GetValue(IsValidProperty);
+        private set => SetValue(IsValidPropertyKey, value);
+    }
+
+    #endregion
+
+    #region ErrorText
+
+    public static readonly DependencyPropertyKey ErrorTextPropertyKey = DependencyProperty.RegisterReadOnly(
+        nameof(ErrorText),
+        typeof(String),
+        typeof(AsnValueEditor),
+        new PropertyMetadata(default(String)));
+
+    public static readonly DependencyProperty ErrorTextProperty = ErrorTextPropertyKey.DependencyProperty;
+
+    public String ErrorText {
+        get => (String)GetValue(ErrorTextProperty);
+        private set => SetValue(ErrorTextPropertyKey, value);
+    }
+
+    #endregion
+
+    protected void SetValidationState(Boolean isValid, String? errorText = "") {
+        IsValid = isValid;
+        ErrorText = isValid
+            ? String.Empty
+            : errorText ?? "The value is not correct.";
+    }
+    protected abstract void OnBinaryValueChanged(Byte[] oldValue, Byte[] newValue);
+}
