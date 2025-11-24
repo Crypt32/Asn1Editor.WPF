6d2694815129f630a37e4ee2856a1eb33cf3a646
Vadims Podāns <vpodans@sysadmins.lv>
2025-11-24 11:33:40 +0200

    Provided implementation for INotifyPropertyChanged. This is deliberately named this way rather than ViewModelBase, which speaks something about WPF-related.

---
+++ src/SysadminsLV.Asn1Editor.Core/NotifyPropertyChanged.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SysadminsLV.Asn1Editor.Core;
+
+/// <summary>
+/// Represents the base implementation of <see cref="INotifyPropertyChanged"/> interface.
+/// </summary>
+public abstract class NotifyPropertyChanged : INotifyPropertyChanged {
+    /// <summary>
+    /// Raises property changed event.
+    /// </summary>
+    /// <param name="propertyName">Property name that changed its value.</param>
+    protected virtual void OnPropertyChanged([CallerMemberName] String? propertyName = null) {
+        PropertyChangedEventHandler handler = PropertyChanged;
+        handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    /// <inheritdoc />
+    public event PropertyChangedEventHandler? PropertyChanged;
+}
