94885739e39713c06289c78cb96afcd235075c3a
Vadims Podāns <vpodans@sysadmins.lv>
2026-02-17 22:16:20 +0200

    handled AsnTreeNode.IsExpanded property via attached dependency property rather than doing wrapping

---
+++ Asn1Editor/API/Utils/WPF/AsnTreeNodeUIBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SysadminsLV.Asn1Editor.API.Utils.WPF;
+
+/// <summary>
+/// Provides attached properties and behaviors for managing the UI behavior of tree nodes in WPF applications.
+/// </summary>
+public static class AsnTreeNodeUIBehavior {
+    public static readonly DependencyProperty IsExpandedProperty =
+        DependencyProperty.RegisterAttached(
+            "IsExpanded",
+            typeof(Boolean),
+            typeof(AsnTreeNodeUIBehavior),
+            new FrameworkPropertyMetadata(
+                true, // Default: expanded
+                FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+                OnIsExpandedChanged
+            )
+        );
+
+    public static Boolean GetIsExpanded(DependencyObject obj) => (Boolean)obj.GetValue(IsExpandedProperty);
+    public static void SetIsExpanded(DependencyObject obj, Boolean value) => obj.SetValue(IsExpandedProperty, value);
+
+    // Sync attached property with TreeViewItem.IsExpanded
+    static void OnIsExpandedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+        if (d is TreeViewItem item && e.NewValue is Boolean isExpanded) {
+            // Prevent infinite loop
+            if (item.IsExpanded != isExpanded) {
+                item.IsExpanded = isExpanded;
+            }
+        }
+    }
+}
