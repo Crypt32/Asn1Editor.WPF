a6a27bce2f49f9f1bae82360dae7264d6a1b6a17
Vadims Podāns <vpodans@sysadmins.lv>
2026-02-18 10:44:55 +0200

    removed ReplaceRange from IBinaryDataSource public interface. Direct manipulations of data source are not allowed outside the TreeCoordinator mediator.

--- src/SysadminsLV.Asn1Editor.Core/Data/IBinarySource.cs
+++ src/SysadminsLV.Asn1Editor.Core/Data/IBinarySource.cs
@@ -12,24 +12,4 @@ namespace SysadminsLV.Asn1Editor.Core.Data;
 /// and <see cref="INotifyPropertyChanged"/> for change notifications, enabling seamless 
 /// integration with data-binding frameworks such as WPF.
 /// </summary>
-public interface IBinarySource : IReadOnlyList<Byte>, INotifyCollectionChanged, INotifyPropertyChanged {
-    /// <summary>
-    /// Replaces a range of bytes in the binary source with the specified new data.
-    /// </summary>
-    /// <param name="offset">
-    /// The zero-based position in the binary source at which the replacement begins.
-    /// </param>
-    /// <param name="count">
-    /// The number of bytes to be replaced starting from the specified <paramref name="offset"/>.
-    /// </param>
-    /// <param name="newData">
-    /// A collection of bytes that will replace the specified range in the binary source.
-    /// </param>
-    /// <exception cref="ArgumentOutOfRangeException">
-    /// Thrown if <paramref name="offset"/> or <paramref name="count"/> is out of the valid range.
-    /// </exception>
-    /// <exception cref="ArgumentNullException">
-    /// Thrown if <paramref name="newData"/> is <c>null</c>.
-    /// </exception>
-    void ReplaceRange(Int32 offset, Int32 count, IReadOnlyCollection<Byte> newData);
-}
+public interface IBinarySource : IReadOnlyList<Byte>, INotifyCollectionChanged, INotifyPropertyChanged;
