1497d3f80dfe55a6d41a21974900ff9e7f7a4714
Vadims Podāns <vpodans@sysadmins.lv>
2026-02-21 20:53:20 +0200

    replaced AsnTextValueEditor with AsnVariantValueEditor, so it reflects actual meaning and implements latest changes in AsnValueEditor

---
+++ Asn1Editor.Wpf.Controls/AsnValueEditors/AsnVariantValueEditor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using SysadminsLV.Asn1Editor.Core.ASN;
+using SysadminsLV.Asn1Parser;
+
+namespace SysadminsLV.Asn1Editor.Controls;
+
+public class AsnVariantValueEditor : AsnValueEditor {
+    AsnValueValidator? validator;
+
+    public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
+        nameof(Value),
+        typeof(String),
+        typeof(AsnVariantValueEditor),
+        new PropertyMetadata(default(String)));
+
+    public String Value {
+        get => (String)GetValue(ValueProperty);
+        set => SetValue(ValueProperty, value);
+    }
+
+    protected override AsnValueValidationResult PerformValidation() {
+        return validator!.Validate(Value);
+    }
+    protected override void OnInputValueChanged(Byte[]? oldValue, Byte[]? newValue) {
+        if (newValue is not null) {
+            validator = AsnValueValidator.Create(newValue[0]);
+            var reader = new Asn1Reader(newValue);
+            var editValue = AsnDecoder.GetEditValue(reader);
+            Value = editValue.TextValue ?? String.Empty;
+
+            SetOutOfBandValidationResult();
+        }
+    }
+
+    static AsnVariantValueEditor() {
+        DefaultStyleKeyProperty.OverrideMetadata(
+            typeof(AsnVariantValueEditor),
+            new FrameworkPropertyMetadata(typeof(AsnVariantValueEditor)));
+    }
+}
