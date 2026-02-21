803d7f7f5b9101db3370b3a0cca85400db8fdcd3
Vadims Podāns <vpodans@sysadmins.lv>
2026-02-21 20:51:12 +0200

    introduced AsnValueValidator class, which is supposed to validate value set in hex mode if hex octet are valid for specified type. Previously, hex values were written without any kind of validation

---
+++ Asn1Editor.Wpf.Controls/AsnValueEditors/AsnValueValidator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using SysadminsLV.Asn1Editor.Core.ASN;
+using SysadminsLV.Asn1Parser;
+
+namespace SysadminsLV.Asn1Editor.Controls;
+
+abstract class AsnValueValidator(Byte tag) {
+    public Byte Tag { get; } = tag;
+
+    public abstract AsnValueValidationResult Validate(Byte[] inputValue);
+    public AsnValueValidationResult Validate(String inputValue) {
+        try {
+            return AsnValueValidationResult.Ok(AsnDecoder.EncodeGeneric(Tag, inputValue, 0));
+        } catch (Exception ex) {
+            return AsnValueValidationResult.Fail(ex.Message);
+        }
+    }
+
+    public static AsnValueValidator Create(Byte tag) {
+        if (tag == (Byte)Asn1Type.BOOLEAN) {
+            return new AsnBooleanTagValidator(tag);
+        }
+
+        return new AsnVariantValidator(tag);
+    }
+}
+sealed class AsnVariantValidator(Byte tag) : AsnValueValidator(tag) {
+    public override AsnValueValidationResult Validate(Byte[] inputValue) {
+        return AsnValueValidationResult.Ok(inputValue);
+    }
+}
+sealed class AsnBooleanTagValidator(Byte tag) : AsnValueValidator(tag) {
+    public override AsnValueValidationResult Validate(Byte[] inputValue) {
+        if (inputValue is not { Length: 3 }) {
+            return AsnValueValidationResult.Fail("BOOLEAN must be a single octet.");
+        }
+
+        return AsnValueValidationResult.Ok(inputValue);
+    }
+}
