d865940060b3db5096c5760ca1ff2c1713274bd7
Vadims Podāns <vpodans@sysadmins.lv>
2026-02-21 20:44:54 +0200

    significantly reworked AsnValueEditor by removing extra dependency properties and replacing it with composite AsnValueValidationResult object, which is passed from control to view model

--- Asn1Editor.Wpf.Controls/AsnValueEditors/AsnValueEditor.cs
+++ Asn1Editor.Wpf.Controls/AsnValueEditors/AsnValueEditor.cs
@@ -2,74 +2,109 @@
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using SysadminsLV.WPF.OfficeTheme.Toolkit.Commands;
 
 namespace SysadminsLV.Asn1Editor.Controls;
 
 public abstract class AsnValueEditor : Control {
-    #region BinaryValue
+    protected AsnValueEditor() {
+        Loaded += onControlLoaded;
+    }
+
+    void onControlLoaded(Object sender, RoutedEventArgs e) {
+        // Expose the validation command to the ViewModel
+        SetCurrentValue(ValidateCommandProperty, new RelayCommand(onValidateCommandExecuted));
+    }
+
+    /// <summary>
+    /// Performs validation of the current ASN.1 value and returns the validation result.
+    /// </summary>
+    /// <remarks>
+    /// This method is invoked by the <see cref="ValidateCommand"/> and is responsible for validating
+    /// the ASN.1 value represented by the editor. The result of the validation is encapsulated in an
+    /// instance of <see cref="AsnValueValidationResult"/>, which provides details about the validity,
+    /// the encoded value (if valid), or an error message (if invalid).
+    /// <para>Inheritors SHALL NOT call this method manually, because it will render no result.</para>
+    /// </remarks>
+    /// <returns>
+    /// An <see cref="AsnValueValidationResult"/> instance containing the outcome of the validation.
+    /// </returns>
+    protected abstract AsnValueValidationResult PerformValidation();
+    /// <summary>
+    /// Sets the validation result for the current control by performing an out-of-band validation.
+    /// </summary>
+    /// <remarks>
+    /// This method invokes the <see cref="PerformValidation"/> method to obtain the validation result
+    /// and updates the <see cref="Result"/> dependency property with the obtained value.
+    /// <para>
+    /// Inheritors MAY call this method to trigger validation and update the result without waiting for trigger from user code.
+    /// It is useful in scenarios where the control needs to perform validation immediately after certain events
+    /// (e.g., after loading or when input value changes) without requiring explicit user action to trigger the validation command.
+    /// </para>
+    /// </remarks>
+    protected void SetOutOfBandValidationResult() {
+        SetCurrentValue(ResultProperty, PerformValidation());
+    }
+
+    #region InputValue
+
     // can't be Byte[]:
     // https://stackoverflow.com/questions/926486/wpf-compilation-error-tags-of-type-propertyarraystart-are-not-supported-in
-    public static readonly DependencyProperty BinaryValueProperty = DependencyProperty.Register(
-        nameof(BinaryValue),
+    public static readonly DependencyProperty InputValueProperty = DependencyProperty.Register(
+        nameof(InputValue),
         typeof(IList<Byte>),
         typeof(AsnValueEditor),
-        new FrameworkPropertyMetadata(null,
-            FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
-            onBinaryValueChanged));
+        new FrameworkPropertyMetadata(null, onInputValueChanged));
+    // must be full TLV value
+    public IList<Byte> InputValue {
+        get => (IList<Byte>)GetValue(InputValueProperty);
+        set => SetValue(InputValueProperty, value);
+    }
 
-    static void onBinaryValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+    static void onInputValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
         if (d is AsnValueEditor editor) {
-            editor.OnBinaryValueChanged((IList<Byte>)e.OldValue, (IList<Byte>)e.NewValue);
+            editor.OnInputValueChanged((Byte[])e.OldValue, (Byte[])e.NewValue);
         }
     }
 
-    public IList<Byte> BinaryValue {
-        get => (IList<Byte>)GetValue(BinaryValueProperty);
-        set => SetValue(BinaryValueProperty, value);
-    }
+    protected abstract void OnInputValueChanged(Byte[]? oldValue, Byte[]? newValue);
 
     #endregion
 
-    #region IsValid
+    #region Result
 
-    static readonly DependencyPropertyKey IsValidPropertyKey =
-        DependencyProperty.RegisterReadOnly(nameof(IsValid),
-            typeof(Boolean),
-            typeof(AsnValueEditor),
-            new PropertyMetadata(true));
-
-
-    public static readonly DependencyProperty IsValidProperty = IsValidPropertyKey.DependencyProperty;
-
-    public Boolean IsValid {
-        get => (Boolean)GetValue(IsValidProperty);
-        private set => SetValue(IsValidPropertyKey, value);
-    }
-
-    #endregion
-
-    #region ErrorText
-
-    public static readonly DependencyPropertyKey ErrorTextPropertyKey = DependencyProperty.RegisterReadOnly(
-        nameof(ErrorText),
-        typeof(String),
+    public static readonly DependencyProperty ResultProperty = DependencyProperty.Register(
+        nameof(Result),
+        typeof(AsnValueValidationResult),
         typeof(AsnValueEditor),
-        new PropertyMetadata(default(String)));
+        new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
 
-    public static readonly DependencyProperty ErrorTextProperty = ErrorTextPropertyKey.DependencyProperty;
-
-    public String ErrorText {
-        get => (String)GetValue(ErrorTextProperty);
-        private set => SetValue(ErrorTextPropertyKey, value);
+    public AsnValueValidationResult Result {
+        get => (AsnValueValidationResult)GetValue(ResultProperty);
+        set => SetValue(ResultProperty, value);
     }
 
     #endregion
 
-    protected void SetValidationState(Boolean isValid, String? errorText = "") {
-        IsValid = isValid;
-        ErrorText = isValid
-            ? String.Empty
-            : errorText ?? "The value is not correct.";
+    #region ValidateCommand
+
+    // expose command to user code to trigger validation from ViewModel. Control itself will set this
+    // command to execute PerformValidation and update Result property.
+    public static readonly DependencyProperty ValidateCommandProperty = DependencyProperty.Register(
+        nameof(ValidateCommand),
+        typeof(ICommand),
+        typeof(AsnValueEditor),
+        new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+    public ICommand ValidateCommand {
+        get => (ICommand)GetValue(ValidateCommandProperty);
+        set => SetValue(ValidateCommandProperty, value);
     }
-    protected abstract void OnBinaryValueChanged(IList<Byte> oldValue, IList<Byte> newValue);
+
+    void onValidateCommandExecuted(Object? o) {
+        SetOutOfBandValidationResult();
+    }
+
+    #endregion
 }
