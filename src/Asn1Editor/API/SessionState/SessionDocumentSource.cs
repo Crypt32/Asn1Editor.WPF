b6b4f4c31b73c0402662006115314292d5a2fc0e
Vadims Podāns <vpodans@sysadmins.lv>
2026-05-28 17:54:37 +0300

    refactored SessionDocumentSource to enable and disable periodic backup creation

--- src/Asn1Editor/API/SessionState/SessionDocumentSource.cs
+++ src/Asn1Editor/API/SessionState/SessionDocumentSource.cs
@@ -9,29 +9,31 @@ namespace SysadminsLV.Asn1Editor.API.SessionState;
 
 /// <summary>
 /// Represents a source for managing session documents within the application.
-/// This class handles the interaction between session tabs and their associated state,
-/// including monitoring changes in the collection of tabs and their properties.
+/// This class handles session recovery options, monitors tab changes, and provides
+/// mechanisms to enable or disable automatic recovery for session documents.
 /// </summary>
 /// <remarks>
-/// The <see cref="SessionDocumentSource"/> class is responsible for saving session states
-/// asynchronously when changes occur in the session tabs or their properties.
-/// It also utilizes a timer to periodically save the session state.
+/// The <see cref="SessionDocumentSource"/> class interacts with the session tab host
+/// and recovery options to ensure proper handling of session states and backups.
+/// It implements the <see cref="IDisposable"/> interface to release resources when no longer needed.
 /// </remarks>
 class SessionDocumentSource : IDisposable {
     readonly ISessionTabHost _sessionTabHost;
     readonly INotifyCollectionChanged _tabsChangeSource;
+    readonly SessionRecoveryOptions _recoveryOptions;
     readonly DispatcherTimer _timer = new();
 
     public SessionDocumentSource(ISessionTabHost sessionTabHost, NodeViewOptions nodeViewOptions) {
         _sessionTabHost = sessionTabHost;
         _tabsChangeSource = sessionTabHost.Tabs;
-        _sessionTabHost.PropertyChanged += SessionTabHost_OnPropertyChanged;
-        _tabsChangeSource.CollectionChanged += TabsChangeSource_OnCollectionChanged;
-        _timer.Tick += Timer_OnTick;
-        _timer.Interval = TimeSpan.FromSeconds(30);
-        //_timer.Start();
+        _recoveryOptions = nodeViewOptions.SessionRecovery;
+        _recoveryOptions.PropertyChanged += RecoveryOptions_OnPropertyChanged;
+        if (_recoveryOptions.EnableAutomaticRecovery) {
+            enableRecovery();
+        }
     }
     
+
 
     async Task saveSessionAsync() {
         _timer.Stop();
@@ -44,6 +46,39 @@ class SessionDocumentSource : IDisposable {
         }
     }
 
+    void enableRecovery() {
+        _sessionTabHost.PropertyChanged += SessionTabHost_OnPropertyChanged;
+        _tabsChangeSource.CollectionChanged += TabsChangeSource_OnCollectionChanged;
+        _timer.Tick += Timer_OnTick;
+        _timer.Interval = getBackupInterval();
+        _timer.Start();
+    }
+    void disableRecovery() {
+        _sessionTabHost.PropertyChanged -= SessionTabHost_OnPropertyChanged;
+        _tabsChangeSource.CollectionChanged -= TabsChangeSource_OnCollectionChanged;
+        _timer.Stop();
+        _timer.Tick -= Timer_OnTick;
+        SessionBackupManager.Instance.Shutdown();
+    }
+    TimeSpan getBackupInterval() {
+        return TimeSpan.FromSeconds(_recoveryOptions.BackupIntervalInSeconds < 0
+            ? 60
+            : _recoveryOptions.BackupIntervalInSeconds);
+    }
+
+    void RecoveryOptions_OnPropertyChanged(Object sender, PropertyChangedEventArgs e) {
+        switch (e.PropertyName) {
+            case nameof(SessionRecoveryOptions.EnableAutomaticRecovery) when _recoveryOptions.EnableAutomaticRecovery:
+                enableRecovery();
+                break;
+            case nameof(SessionRecoveryOptions.EnableAutomaticRecovery):
+                disableRecovery();
+                break;
+            case nameof(SessionRecoveryOptions.BackupIntervalInSeconds):
+                _timer.Interval = getBackupInterval();
+                break;
+        }
+    }
     async void Timer_OnTick(Object sender, EventArgs e) {
         await saveSessionAsync();
     }
@@ -51,18 +86,26 @@ class SessionDocumentSource : IDisposable {
         await saveSessionAsync();
     }
     async void SessionTabHost_OnPropertyChanged(Object sender, PropertyChangedEventArgs args) {
-        await saveSessionAsync();
+        if (args.PropertyName == nameof(ISessionTabHost.SelectedTab)) {
+            await saveSessionAsync();
+        }
     }
 
+    /// <summary>
+    /// Shuts down the session document source and releases associated resources.
+    /// </summary>
+    /// <remarks>
+    /// This method ensures that all session-related resources are properly cleaned up,
+    /// including stopping any ongoing recovery processes and shutting down the
+    /// <see cref="SessionBackupManager"/> instance.
+    /// </remarks>
     public void Shutdown() {
-        SessionBackupManager.Instance.Shutdown();
+        disableRecovery();
     }
 
     /// <inheritdoc />
     public void Dispose() {
-        _sessionTabHost.PropertyChanged -= SessionTabHost_OnPropertyChanged;
-        _tabsChangeSource.CollectionChanged -= TabsChangeSource_OnCollectionChanged;
-        _timer.Stop();
-        _timer.Tick -= Timer_OnTick;
+        _recoveryOptions.PropertyChanged -= RecoveryOptions_OnPropertyChanged;
+        Shutdown();
     }
 }
