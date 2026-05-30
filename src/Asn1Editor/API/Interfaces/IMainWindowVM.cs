bab96405575eedd85ee9a6bae45a7d3077756fcd
Vadims Podāns <vpodans@sysadmins.lv>
2026-05-30 13:40:13 +0300

    removed unused interface method

--- src/Asn1Editor/API/Interfaces/IMainWindowVM.cs
+++ src/Asn1Editor/API/Interfaces/IMainWindowVM.cs
@@ -9,17 +9,7 @@ public interface IMainWindowVM {
     AsnDocumentHostVM? SelectedTab { get; }
 
     /// <summary>
-    /// Raises UI prompt if user wants to save contents of unsaved file.
-    /// </summary>
-    /// <param name="tab">Document to save.</param>
-    /// <returns>
-    ///     <para><strong>True</strong> if user opted to save and save action succeeded or user opted to not save file.</para>
-    ///     <para><strong>False</strong> if user opted to save file and save action failed or user opted to cancel operation.</para>
-    /// </returns>
-    Boolean RequestFileSave(AsnDocumentHostVM tab);
-    /// <summary>
-    /// Requests all tab closing. Internally, this method calls <see cref="RequestFileSave"/> method to prompt to
-    /// save unsaved data.
+    /// Requests all tab closing. Internally, this method calls a prompt to save unsaved data if necessary.
     /// </summary>
     /// <returns>
     ///     <para><strong>True</strong> if user opted to save unsaved files and save action succeeded or user opted to not save file.</para>
