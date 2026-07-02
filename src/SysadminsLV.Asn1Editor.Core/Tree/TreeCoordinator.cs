db2863fe4b33d81dac8127faaade2b7857721a79
Vadims Podāns <vpodans@sysadmins.lv>
2026-07-02 23:12:24 +0300

    Added node status update logic to TreeCoordinator

--- src/SysadminsLV.Asn1Editor.Core/Tree/TreeCoordinator.cs
+++ src/SysadminsLV.Asn1Editor.Core/Tree/TreeCoordinator.cs
@@ -179,6 +179,10 @@ public class TreeCoordinator(INodeViewOptions viewOptions) {
         AsnTreeNode childNode = await AsnTreeBuilder.BuildTreeAsync(nodeRawData, _binarySource, viewOptions);
 
         _binarySource.InsertRange(binaryOffset, nodeRawData);
+        // mark all nodes in the inserted subtree as added
+        foreach (AsnTreeNode node in childNode.Flatten()) {
+            node.Value.Status = AsnNodeStatus.Added;
+        }
         parent.AddChildNode(childNode, insertIndex);
         // this has to be done before propagating size change, because the propagation relies
         // on the node's Path and MyIndex to update offsets of siblings
@@ -217,6 +221,7 @@ public class TreeCoordinator(INodeViewOptions viewOptions) {
 
         _binarySource.RemoveRange(nodeToRemove.Value.Offset, nodeLength);
         parent.RemoveChildNode(nodeToRemove);
+        parent.Value.Status = AsnNodeStatus.Deleted;
         // this has to be done before propagating size change, because the propagation relies
         // on the node's Path and MyIndex to update offsets of siblings
         updatePathsFrom(parent, nodeIndex);
@@ -253,6 +258,7 @@ public class TreeCoordinator(INodeViewOptions viewOptions) {
 
         // Parse the new encoded data to extract metadata
         var asn = new Asn1Reader(newData);
+        node.Value.Status = AsnNodeStatus.Modified;
 
         // Calculate size difference
         Int32 oldNodeSize = node.Value.TagLength;
@@ -282,6 +288,10 @@ public class TreeCoordinator(INodeViewOptions viewOptions) {
         Root!.UpdateNodeView();
     }
 
+    public void ResetMarkers() {
+        Root!.ResetMarkers();
+    }
+
     public void Reset() {
         Root = null;
         _binarySource.Clear();
@@ -309,7 +319,7 @@ public class TreeCoordinator(INodeViewOptions viewOptions) {
     /// <item>
     ///     Any increase in the length field size shifts the binary layout of:
     ///     <list type="bullet">
-    ///         <item>the node’s payload start offset,</item>
+    ///         <item>the nodeâ€™s payload start offset,</item>
     ///         <item>all nodes in its subtree,</item>
     ///         <item>and all following siblings in the parent node.</item>
     ///     </list>
